b914d86dfc2756792ba5528bb1dc50fc1a07fb1f
Joab David Johanan <joabkindle@gmail.com>
2026-02-08 02:39:35 +0100

    feature: add database connection with insert_data function

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+from sqlite3 import Connection
+
+
+# done inside context manager
+# so we do not have to worry about closing the db and cleaning it up
+
+
+def insert_post(connection: Connection, post: dict):
+    with connection:
+        cur = connection.cursor()
+        cur.execute(
+            """
+            INSERT INTO posts (post_title,post_text,user_id)
+            VALUES
+            ( :post_title , :post_text , :user_id  )
+            """,
+            post,
+        )
+
+
+if __name__ == "__main__":
+
+    connection = sqlite3.connect("social.db")
+
+    # dictionary of test post
+    test_post = {
+        "post_title": "Third post",
+        "post_text": "this is a Third post",
+        "user_id": 2,
+    }
+
+    insert_post(connection=connection, post=test_post)
